3053038b46f4a96aeffb198175415c7cb2c2f62c
Atulpagrut <atulpagrut007@gmail.com>
2022-11-23 13:07:45 +0530

    info-way

---
+++ Nov/07-11-2022`/to-do_app/to.js
@@ -0,0 +1,14 @@
+
+
+// creat li
+
+const li=document.createElement("li");
+li.id="listItem"+todos.length;
+ 
+
+//  creat lable
+
+const label=document.createElement("lable");
+label.id="LableId"+todos.length;
+label.style.width="800%";
+ 
